d1c4f02b6c6018158e3a4ff187d0109ae774744b
Igne <125908665+Ignejoneigne@users.noreply.github.com>
2023-10-13 11:05:19 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "aws_vpc" "main" {
 resource "aws_security_group" "sftp_security_group" {
   name        = var.security_group
   description = "Security group"
-  vpc_id      = "vpc-0faf1b0abcce85736"
+  vpc_id      = data.aws_vpc.main.id
 
   ingress {
     from_port   = 15955
@@ -19,7 +19,7 @@ resource "aws_instance" "sftp_server" {
   instance_type = var.instance_type
   key_name      = var.key_pair_name
   security_groups = [var.security_group]
-  iam_instance_profile = "role-d4ml-cloud9-deployment"
+  iam_instance_profile = var.iam_instance_profile_name
   user_data     = <<-EOF
     #!/bin/bash
 
@@ -34,49 +34,6 @@ resource "aws_instance" "sftp_server" {
     EOF
 
   tags = {
-    Name = "SFTP Server"
-    Name        = "SFTP Server"
-    Environment = "D4ML"
-    Owner       = "Igne"
-  }
-}
-data "aws_vpc" "main" {
-  default = true
-}
-
-resource "aws_security_group" "sftp_security_group" {
-  name        = var.security_group
-  description = "Security group"
-  vpc_id      = "vpc-0faf1b0abcce85736"
-
-  ingress {
-    from_port   = 15955
-    to_port     = 15955
-    protocol    = "tcp"
-  }
-}
-
-resource "aws_instance" "sftp_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_pair_name
-  security_groups = [var.security_group]
-  iam_instance_profile = "role-d4ml-cloud9-deployment"
-  user_data     = <<-EOF
-    #!/bin/bash
-
-    while true; do
-      if aws s3 sync "/opt" "s3://${var.s3_bucket_name}/Ignes" --delete; then
-        echo "Backup completed successfully at \$(date)"
-      else
-        echo "Backup failed at \$(date)"
-      fi
-      sleep 60
-    done
-    EOF
-
-  tags = {
-    Name = "SFTP Server"
     Name        = "SFTP Server"
     Environment = "D4ML"
     Owner       = "Igne"
