bdd099faf426c38a4f019f26f3f06e16881bcf49
Igne <125908665+Ignejoneigne@users.noreply.github.com>
2023-10-13 10:49:44 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -6,11 +6,15 @@ sudo apt update
 # Install unzip and git (if not already installed)
 sudo apt install -y unzip git
 
-# Download and install Terraform
-wget https://releases.hashicorp.com/terraform/1.6.1/terraform_1.6.1_linux_amd64.zip
-unzip terraform_1.6.1_linux_amd64.zip
+# Download and install Terraform (latest version)
+wget https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_linux_amd64.zip
+unzip terraform_1.0.0_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-terraform -install-autocomplete
+
+# Install the AWS CLI
+curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 # Clone your Terraform project from GitHub
 git clone https://github.com/Ignejoneigne/Terraform_task_D4ML.git
