2e9fde9c453af8693fc25a939a466e93f2f9042a
Igne <125908665+Ignejoneigne@users.noreply.github.com>
2023-10-13 10:18:31 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+# Update the system
 sudo apt update
 
-# Install Terraform
+# Install unzip and git (if not already installed)
+sudo apt install -y unzip git
+
+# Download and install Terraform
 wget https://releases.hashicorp.com/terraform/1.6.1/terraform_1.6.1_linux_amd64.zip
 unzip terraform_1.6.1_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-terraform --version
+terraform -install-autocomplete
 
-# Create a directory for your project (if it doesn't exist)
-mkdir -p Terraform_task
+# Clone your Terraform project from GitHub
+git clone https://github.com/Ignejoneigne/Terraform_task_D4ML.git
 
-# Change to the project directory and create an opt directory
-cd Terraform_task
-mkdir -p opt
+# Change to the project directory
+cd Terraform_task_D4ML
 
-# Initialize Terraform in your project directory
+# Initialize the Terraform configuration
 terraform init
 
 # Create an execution plan
@@ -28,4 +31,7 @@ terraform apply tfplan
 chmod +x backup_script.sh
 
 # Run the backup script in the background
-nohup ./backup_script.sh &
+nohup ./backup_script.sh > backup_script.log 2>&1 &
+
+# You can check the progress by tailing the log
+tail -f backup_script.log
