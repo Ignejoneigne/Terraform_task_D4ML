3aa478dbde2bad29e41f0c4ba366736cf82d4baf
Igne <125908665+Ignejoneigne@users.noreply.github.com>
2023-10-13 14:09:17 +0300

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -3,7 +3,7 @@
 export S3_BUCKET_NAME="${var.s3_bucket_name}"
 
 while true; do
-  if aws s3 sync "/opt" "s3://$S3_BUCKET_NAME/Ignes" --delete; then
+  if aws s3 sync "/opt" "s3://$S3_BUCKET_NAME/" --delete; then
     echo "Backup completed successfully at $(date)"
   else
     echo "Backup failed at $(date)"
