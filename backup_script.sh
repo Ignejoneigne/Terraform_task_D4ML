66c19373b208fe6316dc45af93db9aed1e2522d1
Igne <125908665+Ignejoneigne@users.noreply.github.com>
2023-10-13 14:25:25 +0300

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-export S3_BUCKET_NAME="${var.s3_bucket_name}"
-
 while true; do
-  if aws s3 sync "/opt" "s3://$S3_BUCKET_NAME/" --delete; then
+  if aws s3 sync "/opt" "s3://${S3_BUCKET_NAME}/" --delete; then
     echo "Backup completed successfully at $(date)"
   else
     echo "Backup failed at $(date)"
