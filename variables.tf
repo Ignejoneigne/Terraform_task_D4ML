6aac5fd01f949035dba7f5865ea08867336ca3cd
Igne <125908665+Ignejoneigne@users.noreply.github.com>
2023-10-13 10:36:59 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,16 @@ variable "aws_region" {
   type        = string
 }
 
+variable "private_ip_cidr" {
+  description = "CIDR block for the private IP address"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket for backups"
+  type        = string
+}
+
 variable "ami_id" {
   description = "ID of the EC2 AMI to use"
   type        = string
@@ -13,8 +23,8 @@ variable "key_pair_name" {
   type        = string
 }
 
-variable "instance_name" {
-  description = "Name of the EC2 instance"
+variable "security_group" {
+  description = "Name of the security group to attach to the EC2 instance"
   type        = string
 }
 
@@ -23,33 +33,7 @@ variable "instance_type" {
   type        = string
 }
 
-
-variable "iam_instance_profile_name" {
-  description = "Name of the IAM instance profile for EC2 instance"
-  type        = string
-}
-
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for backups"
-  type        = string
-}
-
-variable "iam_role_name" {
-  description = "The name of the IAM instance profile role."
-  type        = string
-}
-
-#variable "local_network_cidr" {
-#  description = "CIDR block for the local network"
-#  type        = string
-#}
-
-variable "private_ip_cidr" {
-  description = "CIDR block for the private IP address"
-  type        = string
-}
-
-variable "security_group" {
-  description = "Name of the security group to attach to the EC2 instance."
+variable "instance_name" {
+  description = "Name of the EC2 instance"
   type        = string
 }
